3739bdc924698c5740e660a10aa9a6ca5edf0613
SuperApollo <wpb504@126.com>
2017-02-20 11:53:09 +0800

    1
    1

--- app/src/main/java/com/apollo/weathermvp/app/MyApplication.java
+++ app/src/main/java/com/apollo/weathermvp/app/MyApplication.java
@@ -2,15 +2,26 @@ package com.apollo.weathermvp.app;
 
 import android.app.Application;
 
+import com.apollo.weathermvp.util.volley.VolleyRequest;
+
 /**
  * Created by Apollo on 2017/2/20.
  */
 
 public class MyApplication extends Application {
+    private static MyApplication instance;
 
+    public MyApplication() {
+        instance = this;
+    }
+
+    public static Application getContext() {
+        return instance;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        VolleyRequest.buildRequestQueue(this);
     }
 }
